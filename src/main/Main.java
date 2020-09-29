4f7a77faf4869d175abd373a4adae5b5dcefc13b
Nick <eban7346@gmail.com>
2020-09-29 13:59:16 +0300

    q
    q

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,11 @@ public class Main {
         SkillRepository skillRepository = new SkillRepository();
         Skill skill = new Skill(1L,"Pascal");
         Skill skill1 = new Skill("Java");
-//        System.out.println(skillRepository.getById(2L));
-//        System.out.println(skillRepository.getAll());
-//        System.out.println(skillRepository.save(skill1));
-//        System.out.println(skillRepository.getAll());
-//        skillRepository.deleteById(1L);
+        System.out.println(skillRepository.getById(2L));
+        System.out.println(skillRepository.getAll());
+        System.out.println(skillRepository.save(skill1));
+        System.out.println(skillRepository.getAll());
+        skillRepository.deleteById(1L);
         System.out.println(skillRepository.getAll());
         System.out.println(skillRepository.update(skill));
         System.out.println(skillRepository.getAll());
